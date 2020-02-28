c10b558e8c71592327d95702c071bbe60ca6619e
Joan Manuel Vasquez <yoloprogramo22@gmail.com>
2020-02-28 16:43:08 -0400

    Update removeVowels.js

--- removeVowels.js
+++ removeVowels.js
@@ -3,15 +3,17 @@ const removeVowels = arr => {
   const vowels = ["a", "e", "i", "o", "u"];
 
   const newArr = [];
-  arr.map(word => {
-    for (let i = 0; i < word.length; i++) {
-      if (vowels.includes(word.charAt(i))) {
-        word = word.replace(word.charAt(i), "");
+  // arr.map(word => {
+  for (let f = 0; f < arr.length; f++) {
+    for (let i = 0; i < arr[f].length; i++) {
+      if (vowels.includes(arr[f].charAt(i))) {
+        arr[f] = arr[f].replace(arr[f].charAt(i), "");
       }
     }
-    newArr.push(word);
-  });
-  return newArr;
+    // newArr.push( arr[f]);
+    // });
+  }
+  return arr;
 };
 
 console.log(removeVowels(["camisa", "pedro", "caminar"]));
